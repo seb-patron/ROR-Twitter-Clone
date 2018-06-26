ce833cb919c73b2419e89e85e0e95901b5e3c51e
Sebastian Patron <sebastian.patrones@gmail.com>
2018-06-26 14:24:19 +0200

    changed sign_in/sign_up paths from users/sign_up to #root/sign_up. then created custom path for sign_up as new_user_registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  # Devise uses the default sign_path which is /users/sign_up. The following changes 
+  # the default path to domain.com/sign_up:
+  devise_scope :user do
+     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+     get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+      
+      
   devise_for :users
   resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
